eb6591aeb6ca96b80b6e76c0072e02ade8af107d
Estudiante <Estudiante@DESKTOP-2L5SIDH>
2023-05-03 13:31:54 +0200

    NUevo

--- ProyectoArquitecturaLina/src/Principal.java
+++ ProyectoArquitecturaLina/src/Principal.java
@@ -3,6 +3,12 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Esta es la clase principal de mi proyecto de
+		//para la asignatura de ticII
+		
+		
+		
 
 	}
 
